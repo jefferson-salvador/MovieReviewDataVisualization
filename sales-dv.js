9f7c09a63cada09ea7d2f077cb7b29f1bf730994
Mark Up <jefferson.behionsalvador@gmail.com>
2021-06-05 18:30:09 +0800

    :sparkles: Sales Data Visualization

--- sales-dv.js
+++ sales-dv.js
@@ -1,60 +1,104 @@
-var ctx = document.getElementById('salesChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
 
-    // The data for our dataset
-    data: {
-        labels: ["2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"],
-        datasets: [{
-            label: "",
-            borderColor: '#bc0303',
-            data: ["20000000", "35000000", "47000000", "50000000", "55000000", "57000000", "60000000", "70000000", "75000000", "85000000", "89000000", "87000000", "78000000"],
-            lineTension: 0,
-            pointRadius: 5,
-        }]
-    },
 
-    // Configuration options go here
-    options: {
-        responsive: true,
-        legend: {
-            display: false
-        },
-        tooltips: {
-            callbacks: {
-               label: function(tooltipItem) {
-                      return tooltipItem.yLabel;
-               }
-            }
-        },
 
-        scales: {
-            yAxes: [{
-                ticks: {
-                    fontColor: "white",
-                    beginAtZero: true,
-                    padding: 30
-                },
-                display: true,
-                gridLines: {
-                    zeroLineWidth: 1,
-                    zeroLineColor:'rgba(255, 255, 255, 0.158)',
-                    drawBorder: false,
-                    color: 'rgba(255, 255, 255, 0.158)'
-                }
-            }],
-            xAxes:[{
-                ticks:{
-                    fontColor: "white",
-                }
-            }]
-        },
-        
-        elements:{
-            point: {
-                hitRadius: 5
+function getSales(){
+    var years = [];
+    var sales = [];
+    var titles = [];
+    fetch(`marveldatabase.json`)
+    .then((response)=>{
+        return response.json();
+    })
+    .then((data)=>{
+        var i;
+        var content = document.getElementById("myDiv");
+        for(i = 0 ; i < data.data.length; i++){
+            if(data.data[i].rating != 0){
+                content.insertAdjacentHTML('afterEnd', 
+                `
+                <tr id="mydiv${i}">
+                    <td id="release_date${i}"></td>
+                    <td id="sales${i}"></td>
+                </tr>`
+                )   
+
+                var d = new Date(data.data[i].release_date);
+                var y = d.getFullYear();
+                years.push(y);
+                sales.push(data.data[i].box_office);
+                titles.push(data.data[i].title);
+
+                document.querySelector(`#release_date${i}`).innerHTML = years[i];
+                document.querySelector(`#sales${i}`).innerHTML = '$' + sales[i].toLocaleString();
+                
+                content = document.getElementById(`mydiv${i}`);
             }
         }
-    }
-});
+
+        var ctx = document.getElementById('salesChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+        
+            // The data for our dataset
+            data: {
+                labels: [...years],
+                datasets: [{
+                    label: "",
+                    borderColor: '#bc0303',
+                    data: [...sales],
+                    lineTension: 0,
+                    pointRadius: 5,
+                }]
+            },
+        
+            // Configuration options go here
+            options: {
+                responsive: true,
+                legend: {
+                    display: false
+                },
+                tooltips: {
+                    callbacks: {
+                       label: function(tooltipItem) {
+                              return '$' + tooltipItem.yLabel.toLocaleString() + ' = ' + titles[tooltipItem.index];
+                       }
+                    }
+                },
+        
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            callback: function(tick) {
+                                return '$' + tick.toLocaleString();
+                            },
+                            fontColor: "white",
+                            beginAtZero: true,
+                            padding: 30
+                        },
+                        display: true,
+                        gridLines: {
+                            zeroLineWidth: 1,
+                            zeroLineColor:'rgba(255, 255, 255, 0.158)',
+                            drawBorder: false,
+                            color: 'rgba(255, 255, 255, 0.158)'
+                        }
+                    }],
+                    xAxes:[{
+                        ticks:{
+                            fontColor: "white",
+                        }
+                    }]
+                },
+                
+                elements:{
+                    point: {
+                        hitRadius: 5
+                    }
+                }
+            }
+        });
+
+    });
+}
+
