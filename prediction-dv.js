ab89bc63610fd09526e973e926d5d37c36f88f4d
Mark Up <jefferson.behionsalvador@gmail.com>
2021-06-05 20:15:51 +0800

    :lipstick: Updated prediction styles

--- prediction-dv.js
+++ prediction-dv.js
@@ -2,7 +2,6 @@
 
 function getFutureMovies(){
     var years = [];
-    var ratings = [];
     var titles = [];
     fetch(`marveldatabase.json`)
     .then((response)=>{
@@ -19,23 +18,23 @@ function getFutureMovies(){
                     <img src="" id="cover${i}" class="card-img-top" alt="${data.data[i].title} Cover"></img>
                     <div class="card-body">
                         <h6 id="title${i}" class="card-title"></h6>
-                    </div>
-                    <div class="card-footer">
-                        <span id="release_date${i}"></span>&nbsp;&nbsp;&nbsp;&nbsp;<span id="rating${i}"></span>
+                        <div class="text-left pb-3">
+                            <span id="release_date${i}"></span>
+                        </div>
+                        <small id="casts${i}"></small>
                     </div>
                 </div>`
                 )
                 document.querySelector(`#cover${i}`).src = (data.data[i].cover_url);        
                 document.querySelector(`#title${i}`).innerHTML = data.data[i].title;
                 document.querySelector(`#release_date${i}`).innerHTML = data.data[i].release_date;
-                document.querySelector(`#rating${i}`).innerHTML = (data.data[i].rating) + "%";
+                document.querySelector(`#casts${i}`).innerHTML = (data.data[i].main_cast);
                 
                 content = document.getElementById(`mydiv${i}`);
 
                 var d = new Date(data.data[i].release_date);
                 var y = d.getFullYear();
                 years.push(y);
-                ratings.push(data.data[i].rating);
                 titles.push(data.data[i].title);
             }
         }
